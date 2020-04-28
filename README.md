2303bf429f3017244f083f0644ee534d2b99d9be
joon ko <joonhok@mit.edu>
2020-04-27 20:22:02 -0400

    customizable axes

--- README.md
+++ README.md
@@ -5,5 +5,4 @@
 1. clone and cd into this repository.
 2. `npm install`
 3. `node app.js`
-4. login to spotify with `localhost:8888/login`. after authentication, you should be taken to a callback page with your basic account info.
-5. see your top 25 songs over the last \~6 months with `localhost:8888/tracks`.
+4. open the app in your browser on `localhost:8888`.
--- public/tracks.js
+++ public/tracks.js
@@ -1,4 +1,27 @@
-function displayInfo(d) {
+// use savedData when changing axes so you don't have to call the spotify api again
+let savedData = null;
+
+const width = 1200
+const height = 950
+const margin = ({top: 10, right: 70, bottom: 40, left: 50})
+
+// most audio features use a 0,1 domain, luckily
+const x = d3.scaleLinear()
+  .domain([0, 1])
+  .range([margin.left, width - margin.right])
+const y = d3.scaleLinear()
+  .domain([0, 1])
+  .range([height - margin.bottom, margin.top])
+
+const s = d3.scaleLinear()
+  .domain([0, 24])
+  .range([100, 40])
+
+function displayInfo(d, xField, yField) {
+  // rename 'valence' to more meaningful 'positivity'
+  xFieldName = (xField === 'valence') ? 'positivity' : xField
+  yFieldName = (yField === 'valence') ? 'positivity' : yField
+
   document.getElementById('info').innerHTML = `
     <div><b>${d.track.name}</b></div>
     <div><b>artist:</b> ${getArtists(d.track.artists)}</div>
@@ -6,15 +29,29 @@ function displayInfo(d) {
     <div><b>length:</b> ${formatTime(d.track.duration_ms)}</div>
     <div><b>global popularity:</b> ${d.track.popularity}</div>
     <br>
-    <div><b>danceability:</b> ${d.danceability}</div>
-    <div><b>energy:</b> ${d.energy}</div>
+    <div><b>${yFieldName}:</b> ${d[yField]}</div>
+    <div><b>${xFieldName}:</b> ${d[xField]}</div>
   `
 }
 
 d3.select('#time-range-select').on('change', function() {
   let time_range = d3.select(this).property('value')
-  render(time_range)
+  let xField = d3.select('#x-axis-select').property('value')
+  let yField = d3.select('#y-axis-select').property('value')
+  renderWithCall(time_range, xField, yField)
 })
+
+d3.select('#x-axis-select').on('change', function() {
+  let xField = d3.select(this).property('value')
+  let yField = d3.select('#y-axis-select').property('value')
+  render(xField, yField)
+});
+
+d3.select('#y-axis-select').on('change', function() {
+  let xField = d3.select('#x-axis-select').property('value')
+  let yField = d3.select(this).property('value')
+  render(xField, yField)
+});
 
 function formatTime(ms) {
   let total_seconds = Math.floor(ms / 1000)
@@ -29,7 +66,8 @@ function getArtists(list) {
   return names.join(', ')
 }
 
-function render(time_range) {
+// render with a fresh API call
+function renderWithCall(time_range, xField, yField) {
   axios.get(`/top?time_range=${time_range}`)
     .then(function (response) {
       let trackData = response.data['audio_features']
@@ -37,120 +75,109 @@ function render(time_range) {
         trackData[i].index = i
       }
 
-      let list = d3.select('div#list')
-
-      // clear list
-      list.selectAll('div').remove()
-
-      list.selectAll('div')
-        .data(trackData)
-        .join('div')
-          .text(d => `${d.index + 1}. ${d.track.name}`)
-
-      list.selectAll('div')
-        .data(trackData)
-        .on('mouseover', function(d) {
-          d3.select(this).style('color', 'blue');
-          d3.select('use')
-            .attr('xlink:href', `#i-${d.index}`)
-          d3.selectAll('image')
-            .attr('opacity', 0.3)
-          d3.select(`image#i-${d.index}`)
-            .transition()
-              .duration(200)
-              .ease(d3.easeLinear)
-            .attr('width', d => s(d.index) + 30)
-            .attr('height', d => s(d.index) + 30)
-            .attr('x', d => x(d.energy) - ((s(d.index) + 30)/2))
-            .attr('y', d => y(d.danceability) - ((s(d.index) + 30)/2))
-            .attr('opacity', 1.0)
-          displayInfo(d)
-        })
-        .on('mouseout', function(d) {
-          d3.select(this).style('color', 'black');
-          d3.select(`image#i-${d.index}`)
-            .transition()
-              .duration(200)
-              .ease(d3.easeLinear)
-            .attr('width', d => s(d.index))
-            .attr('height', d => s(d.index))
-            .attr('x', d => x(d.energy) - (s(d.index)/2))
-            .attr('y', d => y(d.danceability) - (s(d.index)/2))
-          d3.selectAll('image')
-            .attr('opacity', 1.0)
-          document.getElementById('info').innerHTML = ''
-        })
-
-      console.log(trackData)
-
-      const width = 1200
-      const height = 950
-      const margin = ({top: 10, right: 70, bottom: 40, left: 50})
-
-      let plot = d3.select('svg#plot')
-        .attr('width', width)
-        .attr('height', height)
-
-      // clear plot
-      plot.selectAll('g').remove()
-      plot.selectAll('use').remove()
-
-      // energy
-      const x = d3.scaleLinear()
-        .domain([0, 1])
-        .range([margin.left, width - margin.right])
-
-      // danceability
-      const y = d3.scaleLinear()
-        .domain([0, 1])
-        .range([height - margin.bottom, margin.top])
-
-      const s = d3.scaleLinear()
-        .domain([0, 24])
-        .range([100, 40])
-
-      // plot axes
-      plot.append('g')
-        .attr('transform', `translate(0, ${height - margin.bottom})`)
-        .call(d3.axisBottom(x))
-        .append('text')
-          .attr('fill', '#000')
-          .attr('transform', 'rotate(0)')
-          .attr('y', margin.bottom)
-          .attr('x', (width + margin.left)/2)
-          .attr('dy', '-0.5em')
-          .attr('font-size', 12)
-          .attr('text-anchor', 'middle')
-          .text('energy');
-      plot.append('g')
-        .attr('transform', `translate(${margin.left}, 0)`)
-        .call(d3.axisLeft(y))
-        .append('text')
-          .attr('fill', '#000')
-          .attr('transform', 'rotate(-90)')
-          .attr('y', -margin.left/2)
-          .attr('x', -height/2)
-          .attr('dy', '-1em')
-          .attr('font-size', 12)
-          .attr('text-anchor', 'middle')
-          .text('danceability');
-
-      plot.append('g')
-          .attr('id', 'points')
-        .selectAll('image')
-        .data(trackData)
-        .join('image')
-          .attr('id', d => `i-${d.index}`)
-          .attr('href', d => d.track.album.images[0].url)
-          .attr('x', d => x(d.energy) - (s(d.index)/2))
-          .attr('y', d => y(d.danceability) - (s(d.index)/2))
-          .attr('width', d => s(d.index))
-          .attr('height', d => s(d.index))
-
-      // used to bring currently highlighted track to front
-      plot.append('use')
-        .attr('xlink:href', '#i-0')
-    });
+      savedData = trackData
+      render(xField, yField)
+    })
 }
 
-render('medium_term')
+function render(xField, yField) {
+  let list = d3.select('div#list')
+
+  // clear list
+  list.selectAll('div').remove()
+
+  list.selectAll('div')
+    .data(savedData)
+    .join('div')
+      .text(d => `${d.index + 1}. ${d.track.name}`)
+
+  list.selectAll('div')
+    .data(savedData)
+    .on('mouseover', function(d) {
+      d3.select(this).style('color', 'blue');
+      d3.select('use')
+        .attr('xlink:href', `#i-${d.index}`)
+      d3.selectAll('image')
+        .attr('opacity', 0.3)
+      d3.select(`image#i-${d.index}`)
+        .transition()
+          .duration(200)
+          .ease(d3.easeLinear)
+        .attr('width', d => s(d.index) + 30)
+        .attr('height', d => s(d.index) + 30)
+        .attr('x', d => x(d[xField]) - ((s(d.index) + 30)/2))
+        .attr('y', d => y(d[yField]) - ((s(d.index) + 30)/2))
+        .attr('opacity', 1.0)
+      displayInfo(d, xField, yField)
+    })
+    .on('mouseout', function(d) {
+      d3.select(this).style('color', 'black');
+      d3.select(`image#i-${d.index}`)
+        .transition()
+          .duration(200)
+          .ease(d3.easeLinear)
+        .attr('width', d => s(d.index))
+        .attr('height', d => s(d.index))
+        .attr('x', d => x(d[xField]) - (s(d.index)/2))
+        .attr('y', d => y(d[yField]) - (s(d.index)/2))
+      d3.selectAll('image')
+        .attr('opacity', 1.0)
+      document.getElementById('info').innerHTML = ''
+    })
+
+  let plot = d3.select('svg#plot')
+    .attr('width', width)
+    .attr('height', height)
+
+  // clear plot
+  plot.selectAll('g').remove()
+  plot.selectAll('use').remove()
+
+  // rename 'valence' to more meaningful 'positivity'
+  xFieldName = (xField === 'valence') ? 'positivity' : xField
+  yFieldName = (yField === 'valence') ? 'positivity' : yField
+
+  // plot axes
+  plot.append('g')
+    .attr('transform', `translate(0, ${height - margin.bottom})`)
+    .call(d3.axisBottom(x))
+    .append('text')
+      .attr('fill', '#000')
+      .attr('transform', 'rotate(0)')
+      .attr('y', margin.bottom)
+      .attr('x', (width + margin.left)/2)
+      .attr('dy', '-0.5em')
+      .attr('font-size', 12)
+      .attr('text-anchor', 'middle')
+      .text(`${xFieldName}`);
+  plot.append('g')
+    .attr('transform', `translate(${margin.left}, 0)`)
+    .call(d3.axisLeft(y))
+    .append('text')
+      .attr('fill', '#000')
+      .attr('transform', 'rotate(-90)')
+      .attr('y', -margin.left/2)
+      .attr('x', -height/2)
+      .attr('dy', '-1em')
+      .attr('font-size', 12)
+      .attr('text-anchor', 'middle')
+      .text(`${yFieldName}`);
+
+  plot.append('g')
+      .attr('id', 'points')
+    .selectAll('image')
+    .data(savedData)
+    .join('image')
+      .attr('id', d => `i-${d.index}`)
+      .attr('href', d => d.track.album.images[0].url)
+      .attr('x', d => x(d[xField]) - (s(d.index)/2))
+      .attr('y', d => y(d[yField]) - (s(d.index)/2))
+      .attr('width', d => s(d.index))
+      .attr('height', d => s(d.index))
+
+  // used to bring currently highlighted track to front
+  plot.append('use')
+    .attr('xlink:href', '#i-0')
+}
+
+renderWithCall('medium_term', 'energy', 'danceability')
